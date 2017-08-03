43bc3169ffcad7777f87ecd06281dee451f392fd
muitsfriday <mu.itsfriday@gmail.com>
2017-08-03 12:02:04 +0700

    step9

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,9 +11,31 @@ class Main extends React.Component {
 		}
 	}
 
-	addScore() {
+	componentWillMount() {
+		console.log('componentWillMount')
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount')
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('shouldComponentUpdate')
+		return parseInt(this.state.score) !== parseInt(nextState.score)
+	}
+
+	componentWillUpdate() {
+		console.log('componentWillUpdate')
+	}
+
+
+	componentDidUpdate() {
+		console.log('componentDidUpdate')
+	}
+
+	addScore(score) {
 		this.setState({
-			score: parseInt(this.state.score) + 50
+			score: parseInt(this.state.score) + score
 		})
 	}
 
@@ -26,12 +48,14 @@ class Main extends React.Component {
 	}
 
 	render() {
+		console.log('render')
 		return (
 			<div id="main-container">
 				<p> score: {this.state.score}</p>
 				{this.renderScore()}
-				{this.props.names.map(name => <HelloWorld name={name == 'Top' ? name + '-Kim' : name} />)}
-				<button onClick={(e) => {this.addScore()}}>click me</button>
+				{this.props.names.map(name => <HelloWorld key={name} name={name == 'Top' ? name + '-Kim' : name} />)}
+				<button onClick={(e) => {this.addScore(50)}}>click me + 50 score</button>
+				<button onClick={(e) => {this.addScore(0)}}>click me + 0 score</button>
 			</div>
 		)
 	}
