6797bdf3b01b1add5fca0522106e65ef5d3e4e58
muitsfriday <mu.itsfriday@gmail.com>
2017-08-03 11:51:29 +0700

    step8

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Main from './components/Main'
-
-
-ReactDom.render(<Main names={['Bell', 'Ann', 'Top', 'Tim']} score="100" />, document.getElementById('app'));
-
