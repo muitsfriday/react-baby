388588efb5b3d6d01fe82f7d28a4b2a85521adba
muitsfriday <mu.itsfriday@gmail.com>
2017-08-18 16:02:45 +0700

    code day2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import registerServiceWorker from './registerServiceWorker'
-
+import registerServiceWorker from './registerServiceWorker' // ไม่เกี่ยว
 
 import App from './App'
 import MagicBox from './components/MagicBox'
@@ -9,7 +8,8 @@ import MagicBox from './components/MagicBox'
 ReactDOM.render(
 <App test={1000} title="hello hello" >
 	<MagicBox names={ ['top', 'bell', 'ann', 'tim'] } />
-</App>, 
-	document.getElementById('root'))
+</App>, document.getElementById('root'))
 
+
+// ไม่เกี่ยว
 registerServiceWorker()
