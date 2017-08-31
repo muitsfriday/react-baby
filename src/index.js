4a3077fa5b3ab02a8d2932591152101f7c44ad78
BuildTools <unconfigured@null.spigotmc.org>
2017-09-01 01:32:15 +0700

    init

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker' // ไม่เกี่ยว
 
+// redux
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducers from './reducers/'
+
 import App from './App'
 
 
+
+const store = createStore(
+	rootReducers, 
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+
 ReactDOM.render(
-	<App />
+	<Provider store={store}>
+		<App />
+	</Provider>	
 , document.getElementById('root'))
 
 
 // ไม่เกี่ยว
 registerServiceWorker()
+
+
+
+/*
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;*/
---
+++ src/reducers/app.js
@@ -0,0 +1,10 @@
+
+
+const initState = {
+	counter: 0,
+	goal: 10
+}
+
+export default (state = initState, action) => {
+	return state
+}
